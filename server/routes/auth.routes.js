5484a1bb113d771c12758aa60dd98741e72f07b4
AlexEvdokimov10 <56792898+AlexEvdokimov10@users.noreply.github.com>
2023-01-21 23:58:03 +0200

    rest fix registration , login

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,10 @@
 const Router=require("express")
 const User=require("../models/User")
 const Bcrypt=require("bcryptjs")
+const Jwt=require("jsonwebtoken")
 const {check,validationResult}=require("express-validator")
 const router=new Router()
+const config=require("config")
 
 router.post("/registration",[check('email',"Uncorrect email").isEmail(), check('password','Password must be longer than 3 and shorter than 12').isLength({min:6,max:12})],async (req, res)=>{
     try{
@@ -15,13 +17,38 @@ router.post("/registration",[check('email',"Uncorrect email").isEmail(), check('
         if(candidate){
             return res.status(400).json({message:`User with email ${email} already exists`})
         }
-        const hashPassword=await Bcrypt.hash(password,15);
-        const user = new User({nickname,email,password})
+        const hashPassword=await Bcrypt.hash(password,8);
+        const user = new User({nickname,email,password:hashPassword})
         await user.save()
         return res.json({message:"User was created"})
 
 
 
+    } catch (e){
+        console.log(e)
+        res.send({message:"Server error"})
+    }
+})
+router.post("/login",async (req, res)=>{
+    try{
+        const {nickname,email,password}=req.body;
+        const user=await User.findOne({email})
+        if(!user) {
+            return res.status(404).json({message:"User doesn't exist"})
+        }
+        const isPassValid=Bcrypt.compareSync(password,user.password)
+        if(!isPassValid){
+            return res.status(400).json({message:"Invalid password"})
+        }
+        const token=Jwt.sign({id:user.id},config.get("secretKey"),{expiresIn:"1h"})
+        return res.json({
+            token,
+            user:{
+                id:user.id,
+                nickname:user.nickname,
+                email:user.email,
+            }
+        })
     } catch (e){
         console.log(e)
         res.send({message:"Server error"})
